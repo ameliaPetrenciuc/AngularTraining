456a5832dddb57d55c5a28293504925652f9eb56
Amelia Petrenciuc <amelia.petrenciuc@gmail.com>
2025-11-19 13:00:50 +0200

    Add online shop backend

---
+++ online-shop-backend/shop/src/main/java/ro/msg/learning/shop/entity/Supplier.java
@@ -0,0 +1,33 @@
+package ro.msg.learning.shop.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import org.hibernate.annotations.UuidGenerator;
+
+import java.util.UUID;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Table(name="supplier")
+public class Supplier {
+    @Id
+    @UuidGenerator
+    private UUID id;
+
+    @Column(name="name")
+    private String name;
+
+    @Column(name="phone_number")
+    private String phoneNumber;
+}
